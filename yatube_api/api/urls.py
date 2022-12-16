52bf1164c2c064f1a191429d876ae83c400cb631
Антон Сободаж <asobodazh@mail.ru>
2022-12-16 09:30:01 +0300

    Second

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -9,7 +9,8 @@ router = SimpleRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'groups', GroupViewSet)
 router.register(r'users', UserViewSet)
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
+router.register(r'posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
